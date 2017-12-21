dc61d85e6ad1de66dc73f8fe67cf666795075da2
Zainal <zainal@gametaiko.com>
2017-12-21 16:15:30 +0800

    Workable moving plane

--- physic_moving_plane.cs
+++ physic_moving_plane.cs
@@ -5,51 +5,60 @@ using UnityEngine;
 public class physic_moving_plane : MonoBehaviour {
 
 	// Use this for initialization
-
 	private Vector2 relativeForce;
 	private Vector2 originalPosition;
 	private bool flipflop;
-
-	[Range(0,1000)]
+	private Transform target;
+	[Range(0,100)]
 	public float Range;
+
+	[Range(0,100)]
+	public float Speed;
+
 	public bool x = true;
 	public bool y = false;
 
 	void Start () {
-		//Hello
 		originalPosition = transform.position;
-
-		if (!x && !y) GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
-		if (!x &&  y) GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionX;
-		if (x  && !y) GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionY;
-		if (x  &&  y) GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.None;
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		if (flipflop) {
-
 			if(x)
-				relativeForce = Vector2.left * Time.deltaTime * 10;
+				relativeForce = Vector2.left * Time.deltaTime * Speed;
 			if(y)
-				relativeForce = Vector2.up * Time.deltaTime * 10;
-
+				relativeForce = Vector2.up * Time.deltaTime * Speed;
 		} else {
 			if(x)
-				relativeForce = Vector2.right * Time.deltaTime * 10;
+				relativeForce = Vector2.right * Time.deltaTime * Speed;
 			if(y)
-				relativeForce = Vector2.down * Time.deltaTime * 10;
-
+				relativeForce = Vector2.down * Time.deltaTime * Speed;
 		}
+			
+		transform.Translate (relativeForce);
 
+		if(target != null)
+		target.Translate (relativeForce);
 
-
-		GetComponent<Rigidbody2D> ().AddRelativeForce (relativeForce);
+		if (transform.position.x > originalPosition.x + Range) {
+			flipflop = true;
+		}
+		if (transform.position.x < originalPosition.x - Range) {
+			flipflop = false;
+		}
+		if (transform.position.y > originalPosition.y + Range) {
+			flipflop = false;
+		}
+		if (transform.position.y < originalPosition.y - Range) {
+			flipflop = true;
+		}
 	}
 
 	void OnCollisionStay2D(Collision2D coll) {
-		coll.transform.parent.GetComponent<Rigidbody2D>().AddRelativeForce(relativeForce);
+		
+		if (coll.gameObject.tag == "Tyre")
+			target = coll.transform.root;
 	}
 }
